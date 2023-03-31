f7d40a722d1e41299aa35120f19a0cae07387d1a
Laboratorio-Info <adrianoramos2401@gmail.com>
2023-03-30 21:33:38 -0300

    Adição de variáveis no java

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MainActivity extends AppCompatActivity {
+    //Declarando variável global que irá representar o xml no java
+    MaterialToolbar idToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Apresentando o xml para o java
+        idToolBar = findViewById(R.id.ToolBar);
     }
 }
